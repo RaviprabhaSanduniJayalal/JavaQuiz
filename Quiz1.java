c97a19d7e9ad0e472948bd66d1f2362fbf856e1c
RaviprabhaSanduniJayalal <151555224+RaviprabhaSanduniJayalal@users.noreply.github.com>
2023-12-14 20:41:50 +0530

    Add files via upload

---
+++ Quiz1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz1;
+
+/**
+ *
+ * @author Raviprabha
+ */
+public class Quiz1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // htis is main class
+        new start().setVisible(true);
+        
+        
+    }
+    
+}
